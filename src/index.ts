34c45c0c10d425e8ed281e7f22ff76ded0c3fd9f
mard <mistermard@protonmail.com>
2022-06-09 10:08:50 +0800

    export ContractCall type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { MultiProvider } from './multi-provider';
 import { Contract } from './contract';
 import { setMulticallAddress } from 'ethers-multicall';
+import { ContractCall } from './types';
 
-export { MultiProvider, Contract, setMulticallAddress };
+export { MultiProvider, Contract, ContractCall, setMulticallAddress };
 export default { MultiProvider, Contract, setMulticallAddress };
